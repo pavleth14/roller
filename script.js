8eed207468b06780173f576ff99d28a9c9ae52f5
pavleth14 <pavleth14@hotmail.com>
2020-03-05 15:58:53 +0100

    change Math.pow() to r*r

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var r = prompt("Enter the radius of roller:");
 var h = prompt("Enter the hight of roller");
 var pi = Math.PI;
 
-var result =  parseInt(Math.pow(r, 2)) * pi * parseInt(h); // kako da umesto r*r napisem re2?
+var result =  parseInt(r*r) * pi * parseInt(h); 
 
 document.write(result.toFixed(2));
